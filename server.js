374073061da22926cad9ed0f1104a7340cc5074c
chlugsdh58 <chlgusdh58@gmail.com>
2022-07-13 15:52:30 +0900

    refactor: cors 코드 관련 수정

--- server.js
+++ server.js
@@ -25,16 +25,6 @@ app.use(useragent.express());
 
 app.use(cors());
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, HEAD, OPTIONS, POST, PUT");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-
 app.use("/auth", login);
 app.use("/api", test);
 app.use("/users", authorization, project);
