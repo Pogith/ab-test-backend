6d3da12ab26f8903b2b38b2301d921b674bf7731
chlugsdh58 <chlgusdh58@gmail.com>
2022-07-17 23:28:58 +0900

    refactor: source.js 를 가독성있게 코드를 작성하고 postRequestData 함수를 재사용할 수 있도록 코드 수정

--- source/source.js
+++ source/source.js
@@ -1,94 +1,91 @@
+const postRequestData = async (url, options) => {
+  const defaultOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+    mode: "no-cors",
+  };
+
+  try {
+    if (options) {
+      await fetch(url, options);
+    } else {
+      await fetch(url, defaultOptions);
+    }
+  } catch (err) {
+    console.error(err);
+  }
+};
+
 window.addEventListener("DOMContentLoaded", async () => {
   const url = window.location.href;
-
   const connectEvent = {
     name: "connect",
     url,
   };
+  const connectEventUrl = `https://abtest.click/api/test-page/${key}?event=${JSON.stringify(connectEvent)}`;
 
-  fetch(
-    `https://abtest.click/api/test-page/${key}?event=${JSON.stringify(connectEvent)}`,
-    {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      credentials: "include",
-      mode: "no-cors"
-    }
-  )
-    .then(() => console.log("receive response!"))
-    .catch((err) => console.error("Connect Error", err));
+  try {
+    await postRequestData(connectEventUrl);
 
-  document.body.addEventListener("click", (event) => {
+    console.log("Connect success");
+  } catch (err) {
+    console.error("Connect Error", err);
+  }
+
+  document.addEventListener("click", async (e) => {
     const clickEvent = {
       name: "click",
-      x: event.pageX,
-      y: event.pageY,
+      x: e.pageX,
+      y: e.pageY,
     };
 
-    fetch(
-      `https://abtest.click/api/test-page/${key}?event=${JSON.stringify(clickEvent)}`,
-      {
-        method: "POST",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-        credentials: "include",
-        mode: "no-cors"
-      }
-    )
-      .then(() => console.log("receive response!"))
-      .catch((err) => console.error("Click Error", err));
+    const clickEventUrl = `https://abtest.click/api/test-page/${key}?event=${JSON.stringify(clickEvent)}`;
+
+    try {
+      await postRequestData(clickEventUrl);
+
+      console.log("Click success");
+    } catch (error) {
+      console.error("Click Error", error);
+    }
   });
 });
 
 if (window.navigator.userAgent.indexOf("Firefox") > -1) {
-  window.onunload = () => {
-    const event = {
+  window.onunload = async () => {
+    const unloadEvent = {
       name: "unload",
     };
+    const unloadEventUrl = `https://abtest.click/api/test-page/${key}?event=${JSON.stringify(unloadEvent)}`;
 
-    fetch(
-      `https://abtest.click/api/test-page/${key}?event=${JSON.stringify(event)}`,
-      {
-        method: "POST",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-        credentials: "include",
-        mode: "no-cors"
-      }
-    )
-      .then(() => console.log("receive response!"))
-      .catch((err) => console.error("Error", err));
+    try {
+      await postRequestData(unloadEventUrl);
+
+      console.log("Unload success");
+    } catch (err) {
+      console.error("Unload Error", err);
+    }
   };
 }
 
 if (window.navigator.userAgent.indexOf("Chrome") > -1) {
-  window.onbeforeunload = (e) => {
+  window.onbeforeunload = async (e) => {
     e.preventDefault();
 
-    const event = {
+    const unloadEvent = {
       name: "unload",
     };
+    const unloadEventUrl = `https://abtest.click/api/test-page/${key}?event=${JSON.stringify(unloadEvent)}`;
 
-    fetch(
-      `https://abtest.click/api/test-page/${key}?event=${JSON.stringify(event)}`,
-      {
-        method: "POST",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-        credentials: "include",
-        mode: "no-cors"
-      }
-    )
-      .then(() => console.log("receive response!"))
-      .catch((err) => console.error("Unload Error", err));
+    try {
+      await postRequestData(unloadEventUrl);
+
+      console.log("Unload success");
+    } catch (err) {
+      console.error("Unload Error", err);
+    }
   };
 }
